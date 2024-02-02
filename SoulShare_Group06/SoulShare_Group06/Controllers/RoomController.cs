8eef6bf53cb8e7498f687e78fa5a44cf9ca1ebde
Thinh Nguyen <nlqthinh.work@gmail.com>
2024-02-02 11:23:16 +0700

    User manage - test chat

---
+++ SoulShare_Group06/SoulShare_Group06/Controllers/RoomController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoulShare_Group06.Controllers
+{
+    public class RoomController : Controller
+    {
+        // GET: Room
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
